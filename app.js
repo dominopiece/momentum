dd949f872d4ecd64a533d52a1b84764cf03f9fa2
dominopiece <dominopiece@gmail.com>
2023-01-27 05:53:30 +0900

    Loading Username

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 const onLoginSubmit = (event) => {
   // 브라우저 기능 방지(새로고침)
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem("username", username)
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+};
+
+const paintGreetings = (username) => {
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 };
@@ -21,3 +26,15 @@ const onLinkClick = (event) => {};
 
 loginForm.addEventListener("submit", onLoginSubmit);
 link.addEventListener("click", onLinkClick);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+// console.log(savedUsername);
+
+if (savedUsername === null) {
+  // show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  // show the greeting
+  paintGreetings(savedUsername);
+}
