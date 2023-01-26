1487636bbba1c98a52268aa5f76e09c56d65c951
dominopiece <dominopiece@gmail.com>
2023-01-27 04:42:01 +0900

    Saving Username

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const onLoginSubmit = (event) => {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
+  localStorage.setItem("username", username)
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 };
